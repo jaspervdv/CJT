8e1011771d4eca1746dd091e4a2ea2f75f4cd64c
jaspervdv <jasper.vaart@live.nl>
2022-12-26 09:47:31 +0100

    Added example2

--- examples/2_CityToOcct/CJTExample2.cpp
+++ examples/2_CityToOcct/CJTExample2.cpp
@@ -1,39 +1,55 @@
 #include <filesystem>
 #include <iostream>
+#include <STEPControl_Writer.hxx>
 
-
+// CJT.h is required for the reading of CityJSON data and the editing of attributes 
 #include <CJT.h>
+
+/// CJToKernel is required for the conversion between the CityJSON format and the OpenCASCADE format
+#include <CJToKernel.h>
 
 int main()
 {
-    // A city collection has to be created. This collection stores all the City(JSON) data in a easily accessable format
+    // As explained in example 1 a city collection has to be created.
     CJT::CityCollection collection;
 
-    /* 
-    The city collection can be populated with the parseJSON method. This function takes a path to the CityJSON fileand a boolean.
-    The boolean dictates if the collection is silenced or not. If the function is silenced it will not communicate in any way.
-    In this case the collection is not silenced, so information will be supplied by methods related to the collection.
-    This can be disabled halfway by calling the silence() method, and reverted by the unsilence() method
+    /*
+    As explained in example 1 the city collection can be populated with the parseJSON method. 
     */
     std::string filepath = std::filesystem::current_path().remove_filename().string();
     collection.parseJSON(filepath + "/twobuildings.city.json", false);
 
     /*
-    A collection is populated by city objects, to change an attribute a city object has to be accessed. 
-    A city object can be selected by the name
+    A kernel object has to be created to be able to convert OpenCASCADE geometry to CityJSON geometry.
+    The kernel constructor requires a reference to the city collection object. 
     */
-    CJT::CityObject* cityObject = collection.getCityObject("Building_1");
-    
-    /*
-    Attributes can be easily added with the addAttribute() function. 
-    In this case we set the key to "CJT Edited?" and the value associated with this key to "Yes!".
-    Instead of two strings a std::pair can also be used 
-    */
-    cityObject->addAttribute("CJT Edited?", "Yes!");
+    CJT::Kernel kernel(&collection);
 
     /*
-    The updated collection now has to be written back to a file.
+    An object that is desired for conversion to the OpenCASCADE format has to be isolated, this is explained in example 1
     */
-    collection.dumpJson(filepath + "/twobuildings_exported.city.json");
+    CJT::CityObject* cityObject1 = collection.getCityObject("Building_1");
+
+    /*
+    The object can be converted to the OpenCASCADE format with the convertToCascade function. This function accepts both a CJT city object or a geo object.
+    The output is a TopoDS shape object. It will attempt to create a TopoDS_Solid, if this is not succesfull it will output a TopoDS_Composit. 
+    */
+
+    std::vector<TopoDS_Shape*> shapeList =  kernel.convertToCascade(*cityObject1);
+
+    /*
+    To visualize the output will be written to a STEP file
+    */
+    STEPControl_Writer writer;
+    
+    for (std::vector<TopoDS_Shape*>::iterator ptr = shapeList.begin(); ptr < shapeList.end(); ptr++)
+    {
+        TopoDS_Shape* geoObject = *ptr;
+        writer.Transfer(*geoObject, STEPControl_AsIs);
+    }
+
+    std::string exportPath = filepath + "/twobuildings_step_export.step";
+    writer.Write(exportPath.c_str());
+
 }
 
